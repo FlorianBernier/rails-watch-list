52560c9d96ede8d81ecb2481cb57849ad12ff767
florian bernier <florianbernier.soa@gmail.com>
2024-02-23 15:50:00 +0100

    scss index

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,39 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+  before_action :set_bookmark, only: [:destroy]
+
+  def new
+    @bookmark = Bookmark.new
+    @movies = Movie.all - @list.movies
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to @list, notice: 'Bookmark was successfully created.'
+    else
+      @movies = Movie.all - @list.movies
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to @bookmark.list, notice: 'Bookmark was successfully destroyed.'
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+end
